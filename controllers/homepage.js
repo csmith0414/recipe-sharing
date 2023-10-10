558bb79114a3644455d10b09391a67845c89e74e
Chad Smith <cds021@yahoo.com>
2023-10-10 18:44:02 -0400

    update recipe routes

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const { Recipe } = require('../../models');
-const withAuth = require('../../utils/auth');
+const { Recipe } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
