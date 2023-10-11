3312b6e138a47aff8dca89164768c27be268a6dc
Anthony Iacano <iacano5@outlook.com>
2023-10-10 21:26:27 -0400

    added change to signup section

--- public/js/login.js
+++ public/js/login.js
@@ -32,7 +32,7 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#password-signup').value.trim();
   
     if (name && email && password) {
-      const response = await fetch('/api/userRoutes', {
+      const response = await fetch('/api/users', {
         method: 'POST',
         body: JSON.stringify({ name, email, password }),
         headers: { 'Content-Type': 'application/json' },
